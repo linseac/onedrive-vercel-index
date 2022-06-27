e7e2249d4339deb8ccecd38ea2c0dc36319cf74c
linseac <108252241+linseac@users.noreply.github.com>
2022-06-27 20:16:44 +0800

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -39,7 +39,7 @@ module.exports = {
 
   // [OPTIONAL] This is where you specify the folders that are password protected. It is an array of paths pointing to all
   // the directories in which you have .password set. Check the documentation for details.
-  protectedRoutes: ["/yunpan/个人资料",],
+  protectedRoutes: ["/个人资料",],
 
   // [OPTIONAL] Use "" here if you want to remove this email address from the nav bar.
   email: 'mailto:alinseac@163.com',
