ada842c19463cebd09fef1bc12ab8bc6ff859706
linseac <108252241+linseac@users.noreply.github.com>
2022-06-27 19:54:13 +0800

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -50,7 +50,7 @@ module.exports = {
   links: [
     {
       name: 'GitHub',
-      link: 'https://github.com/spencerwooo/onedrive-vercel-index',
+      link: 'https://github.com/linseac',
     },
     {
       name: '网易云音乐',
